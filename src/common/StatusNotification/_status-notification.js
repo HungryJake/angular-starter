a23cb2fa9f3ba81f7abe2d4398017b2980203a4d
Jeremy Lu <DrkCoater@gmail.com>
2018-04-29 14:03:38 -0700

    remove common

--- src/common/StatusNotification/_status-notification.js
+++
@@ -1,7 +0,0 @@
-angular.module('app', [])
-    .config(function() {
-        // configure
-    })
-    .run(function() {
-        // run
-    });
--- src/common/StatusNotification/status-notification.directive.js
+++
@@ -1,7 +0,0 @@
-angular.module('app')
-    .directive(function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'views/status-notification.html'
-        };
-    });
